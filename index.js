d783ee08afdc23b76b2f62b0e0b278406cac99ce
Camper76 <68886028+Camper76@users.noreply.github.com>
2020-08-10 14:29:09 +0700

    add file
    tahap1

---
+++ index.js
@@ -0,0 +1,47 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('motion-1'),
+  renderer: 'svg',
+  loop:true,
+  autoplay:true,
+  path:'page 1/data.json'
+})
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('motion-3'),
+  renderer: 'svg',
+  loop:true,
+  autoplay:true,
+  path:'page 3/data.json'
+})
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('motion-4'),
+  renderer: 'svg',
+  loop:true,
+  autoplay:true,
+  path:'page 4/data.json'
+})
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('motion-5'),
+  renderer: 'svg',
+  loop:true,
+  autoplay:true,
+  path:'page 5/data.json'
+})
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('motion-ttg'),
+  renderer: 'svg',
+  loop:true,
+  autoplay:true,
+  path:'tentang/data.json'
+})
+
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('loading'),
+  renderer: 'svg',
+  loop:true,
+  autoplay:true,
+  path:'loading/data.json'
+})
